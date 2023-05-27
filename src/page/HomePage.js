275bf1a7c4bef9e0e3583097450671b4ee464f9b
N.S.Hoàng <122682692+Sihoang0702@users.noreply.github.com>
2023-05-27 21:22:23 +0700

    Update HomePage.js

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,6 +1,6 @@
 import { Banner } from "../components/Banner";
 import { Portfolio } from "../components/Portfolio";
-import { Header } from "../components/header";
+import { Header } from "../components/Header";
 import { Skills } from "../components/Skills";
 import { Goals } from "../components/Goals";
 import { Footer } from "../components/Footer";
